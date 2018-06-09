1577cd80531cbd51b8e45b16fa1946794e07cb42
GT~77 <35972785+GT-77@users.noreply.github.com>
2018-06-09 14:12:32 +0300

    Initial commit

---
+++ neural_networks.py
@@ -0,0 +1,34 @@
+from random import randint
+def _signal(*signals):
+    returning = ()
+    sum_ = 0.0
+    for index in range(len(signals[1][0])):
+        for (index2, neuron) in enumerate(signals[0]):
+            sum_ += neuron * signals[1][index2][index]
+        returning += (sum_ / len(signals[0]),)
+        sum_ = 0.0
+    return(returning)
+class NeuralNetwork(object):
+    def __init__(self, *neurons, random = False):
+        self.layers = []
+        for (index, neuron_count) in enumerate(neurons):
+            self.layers.append([])
+            for neuron in range(neuron_count):
+                self.layers[index].append([])
+                try:
+                    neurons[index + 1]
+                except(IndexError):
+                    pass
+                else:
+                    for i in range(neurons[index + 1]):
+                        if random:
+                            self.layers[index][neuron].append(randint(-100, 100) / 100)
+                        else:
+                            self.layers[index][neuron].append(0.0)
+    def __call__(self, *args):
+        if len(args) != len(self.layers[0]):
+            raise(TypeError(str(len(args)) + " arguments were given to a neural network that has " + str(len(self.layers[0])) + " input neurons"))
+        signaling = _signal(args, tuple(self.layers[0]))
+        for layer in self.layers[1: -1]:
+            signaling = _signal(signaling, tuple(layer))
+        return(signaling)
